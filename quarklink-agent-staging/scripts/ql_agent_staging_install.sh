0648dad282c88bc8ab005bed8d87deeff855a8b7
Claudia Mura <CMura@cryptoquantique.com>
2024-06-05 15:36:12 +0100

    added prompt key

--- quarklink-agent-staging/scripts/ql_agent_staging_install.sh
+++ quarklink-agent-staging/scripts/ql_agent_staging_install.sh
@@ -32,25 +32,7 @@ read_provision_details () {
   read instance
   echo $instance > $quarklink_config_dir/ql_endpoint
 
-  #read root certificate
-  echo "Enter the root certificate (you may have to press enter twice)"
-  # [ -f "$quarklink_config_dir/ql_ca_cert.pem" ] && rm "$quarklink_config_dir/ql_ca_cert.pem"
-  while read line
-  do
-    # break if the line is empty
-    [ -z "$line" ] && break
-    echo "$line" >> $quarklink_config_dir/ql_ca_cert.pem
-  done
 
-  #read signing key
-  echo "Enter the root certificate (you may have to press enter twice)"
-  # [ -f "$quarklink_config_dir/signing key.pem" ] && rm "$quarklink_config_dir/signing key.pem"
-  while read line
-  do
-    # break if the line is empty
-    [ -z "$line" ] && break
-    echo "$line" >> $quarklink_config_dir/signing key.pem
-  done
 }
 
 # install_agent function will install the agent onto the machine
