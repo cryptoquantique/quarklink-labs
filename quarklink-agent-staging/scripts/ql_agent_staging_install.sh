0a048540633a2005f05abd9ef34ff026b3f9b659
Claudia Mura <CMura@cryptoquantique.com>
2024-06-05 14:55:46 +0100

    added prompt key

--- quarklink-agent-staging/scripts/ql_agent_staging_install.sh
+++ quarklink-agent-staging/scripts/ql_agent_staging_install.sh
@@ -34,9 +34,8 @@ read_provision_details () {
 
   #read root certificate
   echo "Enter the root certificate (you may have to press enter twice)"
-  if [ -z "$line" ] && break
-  fi
-    rm $quarklink_config_dir/ql_ca_cert.pem
+  [ -z "$line" ] && break
+  rm $quarklink_config_dir/ql_ca_cert.pem
     while read -r line
     do
       # break if the line is empty
