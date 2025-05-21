9cee70e322692e008a578206f48a65174279f77a
Crypto Quantique Dev Team <142031521+cryptoquantique-dev@users.noreply.github.com>
2025-05-21 10:45:58 +0100

    Update quarklink-agent install script

--- quarklink-agent/scripts/ql_agent_install.sh
+++ quarklink-agent/scripts/ql_agent_install.sh
@@ -10,13 +10,14 @@ agent_service="https://raw.githubusercontent.com/cryptoquantique/quarklink-labs/
 agent_config="https://raw.githubusercontent.com/cryptoquantique/quarklink-labs/main/quarklink-agent/service/config.yaml"
 quarklink_config_dir="/etc/quarklink"
 ssh_user="quarklink"
+agent_path="/usr/bin/quarklink-agent"
 
 # start_agent function will enable the agent to start on boot and start the agent
 start_agent () {
   systemctl enable quarklink-agent
   systemctl start quarklink-agent
   if [ "$?" -ne 0 ]; then
-    echo "Quarklink agent failed to start, check whether device id added to batch etc."
+    echo "Quarklink agent failed to start."
     exit 1
   fi
   echo "Quarklink agent successfully started."
@@ -45,17 +46,7 @@ read_provision_details () {
     [ -z "$line" ] && break
     echo "$line" >> $quarklink_config_dir/ql_ca_cert.pem
   done
-
-  [ -f "$quarklink_config_dir/ql_sign_key.pem" ] && rm "$quarklink_config_dir/ql_sign_key.pem"
-  #read signing key
-  echo "Enter the signing key (you may have to press enter twice)"
-  while read -r line
-  do
-    # break if the line is empty
-    [ -z "$line" ] && break
-    echo "$line" >> $quarklink_config_dir/ql_sign_key.pem
-  done
-  }
+}
 
 # install_agent function will install the agent onto the machine
 install_agent () {
@@ -68,19 +59,19 @@ install_agent () {
   case $sys_type in
     x86_64)
       echo "64-bit system x86_64"
-      curl -o /usr/bin/quarklink-agent $amd64_agent
+      curl -o $agent_path $amd64_agent
       ;;
     aarch64)
       echo "64 bit arm system aarch64"
-      curl -o /usr/bin/quarklink-agent $arm64_agent
+      curl -o $agent_path $arm64_agent
       ;;
     armv7l)
       echo "32 bit arm system armv7l"
-      curl -o /usr/bin/quarklink-agent $arm32_agent
+      curl -o $agent_path $arm32_agent
       ;;
     amd64)
       echo "64 bit amd system amd64"
-      curl -o /usr/bin/quarklink-agent $amd64_agent
+      curl -o $agent_path $amd64_agent
       ;;
     *)
       echo "Unsupported system type"
@@ -88,18 +79,20 @@ install_agent () {
       ;;
   esac
 
+  #make it executable
+  chmod +x $agent_path
+
   #copy configs and service descriptions.
   curl -o $quarklink_config_dir/agent/config.yaml $agent_config
   curl -o /etc/systemd/system/quarklink-agent.service $agent_service
 }
 
-# run_agent will start running the agent
-run_agent () {
+# get_device_id prints the device ID
+get_device_id () {
   #run the agent
-  chmod +x /usr/bin/quarklink-agent
-  /usr/bin/quarklink-agent -crypto=local -deviceID
+  $agent_path -deviceID
   if [ "$?" -ne 0 ]; then
-      echo "Quarklink agent failed to start, check whether device id added to batch etc."
+      echo "Failed to run quarklink agent"
       exit 1
   fi
 }
@@ -108,40 +101,43 @@ create_ssh_user() {
   if [ ! -d /home/$ssh_user ]; then
     useradd -m -s /bin/bash $ssh_user
     usermod -a -G sudo $ssh_user
-    mkdir /home/$ssh_user/.ssh/
-    ssh-keygen -t rsa -b 4096 -f /home/$ssh_user/.ssh/id_rsa -q -N ""
   fi 
 }
 
 # Check if the binary already exists
-if [ -e /usr/bin/quarklink-agent ]; then
-  echo "Quarklink agent has already been installed, Would you like to reinstall or reprovision?"
-  echo "reinstall/reprovision"
+if [ -e $agent_path ]; then
+  echo "Quarklink agent is already installed."
+  echo "What would you like to do?"
+  echo "1. Reinstall"
+  echo "2. Reconfigure"
+  echo "3. Cancel"
   printf "> "
   read inputCommand 
-  if [ "$inputCommand" = "reinstall" ]; then
+  if [ "$inputCommand" = "1" ]; then
     echo
     echo "Reinstalling Quarklink-Agent"
     rm -r $quarklink_config_dir
     systemctl stop quarklink-agent
     install_agent
     start_agent
-  elif [ "$inputCommand" = "reprovision" ]; then
+  elif [ "$inputCommand" = "2" ]; then
     echo
-    echo "Reprovisioning with quarklink"
+    echo "Reconfiguring QuarkLink details"
     read_provision_details
     start_agent
+  elif [ "$inputCommand" = "3" ]; then
+    echo
+    echo "Cancelling, no action performed"
   fi
-  echo "Cancelling, no action performed."
   exit
 fi
 
 # Full initial install
-echo "...Installing quarklink agent..."
+echo "Installing quarklink agent..."
 
 # Check if root ca file exists
 if [ -f $quarklink_config_dir/ql_ca_cert.pem ]; then
-  echo "Root certificate already exists, seems like already provisioned, trying to start agent"
+  echo "Looks like a configuration already exists, starting existing agent service."
   start_agent
 fi
 
@@ -151,7 +147,10 @@ create_ssh_user
 # Install agent
 install_agent
 
-# Start the agent and enable it to start on boot
+# Print the device ID
+get_device_id
+echo "Add this device ID to a batch to successfully run the agent"
+
+# Start the agent service and enable it to start on boot
 start_agent
 '
-
